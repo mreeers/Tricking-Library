6b41d5df500c1a35f6d1ceda2132dffed525cbc8
Александр Ефанов <verpl01@yandex.ru>
2020-09-24 14:59:54 +0500

    ASP.NET Core API & Nuxtjs SSR in Rider - Project Setup - Tricking Library Ep1

---
+++ TrickingLibrary.API/Controllers/Home.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Home : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello world";
+        }
+    }
+}
