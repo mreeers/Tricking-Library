e12dae8d47d364f9e2382ecc9aada97f41ed723c
Александр Ефанов <verpl01@yandex.ru>
2020-12-02 12:30:58 +0500

    ASP.NET Core, C#, Nuxtjs, Regex - Comments - Tricking Library Ep24

--- TrickingLibrary.API/Controllers/ModerationItemController.cs
+++ TrickingLibrary.API/Controllers/ModerationItemController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TrickingLibrary.Data;
+using TrickingLibrary.Models;
 using TrickingLibrary.Models.Moderation;
 
 namespace TrickingLibrary.API.Controllers
@@ -26,5 +29,35 @@ namespace TrickingLibrary.API.Controllers
         [HttpGet("{id}")]
         public ModerationItem Get(int id) =>
             _context.ModerationItems.FirstOrDefault(x => x.Id.Equals(id));
+
+        [HttpGet("{id}/comments")]
+        public IEnumerable<Comment> GetComments(int id) =>
+            _context.ModerationItems
+                .Include(x => x.Comments)
+                .Where(x => x.Id.Equals(id))
+                .Select(x => x.Comments)
+                .FirstOrDefault();
+
+        [HttpPost("{id}/comments")]
+        public async Task<IActionResult> Comment(int id, [FromBody] Comment comment)
+        {
+            var modItem = _context.ModerationItems.FirstOrDefault(x => x.Id == id);
+
+            var regex = new Regex(@"\B(?<tag>@[a-zA-Z0-9-_]+)");
+
+            comment.HtmlContent = comment.Content;
+
+            comment.HtmlContent = regex.Matches(comment.Content)
+                                       .Aggregate(comment.Content, (content, match) =>
+                                        {
+                                            var tag = match.Groups["tag"].Value;
+                                            return content.Replace(tag, $"<a href=\"{tag}-user-link\">{tag}</a>");
+                                        });
+
+            modItem.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+
+            return Ok(comment);
+        }
     }
 }
---
+++ TrickingLibrary.Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrickingLibrary.Models
+{
+    public class Comment : BaseModel<int>
+    {
+        public string Content { get; set; }
+        public string HtmlContent { get; set; }
+    }
+}
